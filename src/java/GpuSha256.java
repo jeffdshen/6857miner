3587e579addfc979d855978a003bdf35f48fa9b8
jdshen <jdshen@mit.edu>
2015-02-26 00:07:45 -0500

    first commit

---
+++ src/java/GpuSha256.java
@@ -0,0 +1,317 @@
+import com.amd.aparapi.Kernel;
+
+/**
+* SHA-256 hash algorithm for Monetary System currencies
+*
+* GpuSha256 uses the Aparapi package to calculate hashes using the graphics card
+* GPU.  The Aparapi and OpenCL runtime libraries must be available in either the
+* system path or the Java library path.  This GPU version of HashSha256 is optimized
+* for the GPU and does not support generalized hashing.
+*
+* Aparapi builds an OpenCL kernel from the Java bytecodes when the Kernel.execute()
+* method is invoked.  The compiled program is saved for subsequent executions by
+* the same kernel.
+*
+* Aparapi supports a subset of Java functions:
+*   o Only primitive data types and single-dimension arrays are supported.
+*   o Objects cannot be created by the kernel program.  This means that kernel
+*     code cannot use the 'new' operation.
+*   o Java exceptions, enhanced 'for' statements and 'break' statements are not supported.
+*   o Variable assignment during expression evaluation is not supported.
+*   o Primitive data types defined within a function must be assigned a value at the time of definition.
+*   o Only data belonging to the enclosing Java class can be copied to/from GPU memory.
+*   o Primitives and objects can be read from kernel code but only objects can be
+*     written by kernel code.
+*   o Aparapi normally defines kernel groups based on the capabilities of the graphics card.
+*     The Range object can be used to define an explicit grouping.
+*   o Untagged data is shared by all instances of the kernel.
+*   o Constant data is not fetched upon completion of kernel execution.  Constant data is
+*     indicated by prefixing the data definition with @Constant.
+*   o Local data is shared by all instances in the same kernel group.  Local data is indicated
+*     by prefixing the data definition with @Local
+*   o Private data is not shared and each kernel instance has its own copy of the data.  Private
+*     data is indicated by prefixing the data definition with @PrivateMemorySpace(nnn) where 'nnn'
+*     is the array dimension.  Private data cannot be passed as a function parameter since it is
+*     in a separate address space and this information is not passed on the function call.
+*/
+public class GpuSha256 extends Kernel {
+
+    /** SHA-256 constants */
+    private final int[] k = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+
+    /** Input data */
+    private final byte[] input = new byte[64];
+
+    /** Hash target */
+    public final byte[] target = new byte[32];
+
+    private final byte[] block;
+    private final long start;
+    private final long mod;
+    private final int length;
+    public final boolean[] solved = new boolean[1];
+    public final long[] nonce = new long[1];
+
+    /**
+     * Create the GPU hash function
+     *
+     */
+    public GpuSha256(byte[] block, long start, long mod, int length) {
+        this.start = start;
+        this.mod = mod;
+        this.length = length;
+        this.block = block;
+        setInput(block);
+    }
+
+//    public GpuSha256() {
+//
+//    }
+
+    /**
+     * Set the input data and the hash target
+     *
+     * The input data is in the following format:
+     *     Bytes 0-7:   Initial nonce (modified for each kernel instance)
+     *     Bytes 8-15:  Currency identifier
+     *     Bytes 16-23: Currency units
+     *     Bytes 24-31: Minting counter
+     *     Bytes 32-39: Account identifier
+     *
+     * The hash target and hash digest are treated as unsigned 32-byte numbers in little-endian format.
+     * The digest must be less than the target in order to be a solution.
+     *
+     * @param       inputBytes      Bytes to be hashed (40 bytes)
+     */
+    public void setInput(byte[] inputBytes) {
+//        if (inputBytes.length != 46)
+//            throw new IllegalArgumentException("Input data length must be 40 bytes");
+        //
+        // Copy the input data
+        //
+
+
+        // CUSTOM : assumes content = 2 bytes, and thus we have 46 total bytes
+        System.arraycopy(inputBytes, 0, input, 0, 46);
+        //
+        // Pad the buffer
+        //
+        // SHA-256 processes data in 64-byte blocks where the data bit count
+        // is stored in the last 8 bytes in big-endian format.  The first pad
+        // byte is 0x80 and the remaining pad bytes are 0x00.  Since we have
+        // 46 bytes of data, the data bit count is 368 (0x170).
+        //
+        input[46] = (byte)0x80;
+        input[62] = (byte)0x01;
+        input[63] = (byte)0x70;
+
+        // clear targetBytes
+        for (int i = 0; i < 32; i++) {
+            target[i] = 0;
+        }
+
+    }
+
+    /**
+     * Compute the hash
+     */
+    public boolean hash(long nonce, int difficulty) {
+        //
+        // Initialization
+        //
+        int A = 0x6a09e667;
+        int B = 0xbb67ae85;
+        int C = 0x3c6ef372;
+        int D = 0xa54ff53a;
+        int E = 0x510e527f;
+        int F = 0x9b05688c;
+        int G = 0x1f83d9ab;
+        int H = 0x5be0cd19;
+        int r, T, T2;
+        int offset = 0;
+        int w0=0, w1=0, w2=0, w3=0, w4=0, w5=0, w6=0, w7=0, w8=0, w9=0, w10=0;
+        int w11=0, w12=0, w13=0, w14=0, w15=0, w16=0;
+
+        long value = nonce;
+        byte nonce7 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce6 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce5 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce4 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce3 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce2 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce1 = (byte) (value & 0xffL);
+        value >>= 8;
+        byte nonce0 = (byte) (value & 0xffL);
+        value >>= 8;
+        // Transform the data
+        //
+        // We will modify the nonce (first 8 bytes of the input data) for each execution instance
+        //
+        for (r=0; r<64; r++) {
+            if (r < 16) {
+                if (r == 8) {
+                    // CUSTOM : nonce at bytes 34, 35 (34 / 4 = 8)
+                    w16 = (input[offset++] << 24 | (input[offset++] & 0xFF) << 16 |
+                        (nonce0 & 0xFF) << 8 | (nonce1 & 0xFF));
+                    offset++;
+                    offset++;
+                } else if (r == 9) {
+                    // CUSTOM : nonce at byte 36, 37, 38, 39 (36 / 4 = 9)
+                    w16 = (nonce2 << 24 | (nonce3 & 0xFF) << 16 |
+                        (nonce4 & 0xFF) << 8 | (nonce5 & 0xFF));
+                    offset++;
+                    offset++;
+                    offset++;
+                    offset++;
+                } else if (r == 10) {
+                    offset++;
+                    offset++;
+                    w16 = (nonce6 << 24 | (nonce7 & 0xFF) << 16 |
+                        (input[offset++] & 0xFF) << 8 | (input[offset++] & 0xFF));
+                } else {
+                    w16 = (input[offset++] << 24 | (input[offset++] & 0xFF) << 16 |
+                        (input[offset++] & 0xFF) << 8 | (input[offset++] & 0xFF));
+                }
+            } else {
+                T =  w14;
+                T2 = w1;
+                w16 = ((((T >>> 17) | (T << 15)) ^ ((T >>> 19) | (T << 13)) ^ (T >>> 10)) + w9 +
+                    (((T2 >>> 7) | (T2 << 25)) ^ ((T2 >>> 18) | (T2 << 14)) ^ (T2 >>> 3)) + w0);
+            }
+            T = (H + (((E >>> 6) | (E << 26)) ^ ((E >>> 11) | (E << 21)) ^ ((E >>> 25) | (E << 7))) +
+                ((E & F) ^ (~E & G)) + k[r] + w16);
+            T2 = ((((A >>> 2) | (A << 30)) ^ ((A >>> 13) | (A << 19)) ^ ((A >>> 22) | (A << 10))) +
+                ((A & B) ^ (A & C) ^ (B & C)));
+            w0 = w1; w1 = w2; w2 = w3; w3 = w4; w4 = w5; w5 = w6; w6 = w7; w7 = w8; w8 = w9;
+            w9 = w10; w10 = w11; w11 = w12; w12 = w13; w13 = w14; w14 = w15; w15 = w16;
+            H = G; G = F; F = E;
+            E = D + T;
+            D = C; C = B; B = A;
+            A = T + T2;
+        }
+        //
+        // Finish the digest
+        //
+        int h0 = A + 0x6a09e667;
+        int h1 = B + 0xbb67ae85;
+        int h2 = C + 0x3c6ef372;
+        int h3 = D + 0xa54ff53a;
+        int h4 = E + 0x510e527f;
+        int h5 = F + 0x9b05688c;
+        int h6 = G + 0x1f83d9ab;
+        int h7 = H + 0x5be0cd19;
+
+        int cur = 0;
+        if (difficulty >= 32) {
+            if (h0 != 0) {
+                return false;
+            }
+
+            if (difficulty >= 64) {
+                if (h2 != 0) {
+                    return false;
+                }
+
+                if (difficulty >= 72) {
+                    if (h3 != 0) {
+                        return false;
+                    }
+                    // should never reach...
+                    cur = h3;
+                } else {
+                    cur = h2;
+                }
+            } else {
+                cur = h1;
+            }
+        } else {
+            cur = h0;
+        }
+
+        difficulty %= 32;
+        if ((cur >> (32 - difficulty)) != 0) {
+            return false;
+        }
+
+        // if made it here, then the check worked
+
+        target[0] = (byte) (h0 >> 24);
+        target[1] = (byte) (h0 >> 16);
+        target[2] = (byte) (h0 >> 8);
+        target[3] = (byte) (h0);
+
+        target[4] = (byte) (h1 >> 24);
+        target[5] = (byte) (h1 >> 16);
+        target[6] = (byte) (h1 >> 8);
+        target[7] = (byte) (h1);
+
+        target[8] = (byte) (h2 >> 24);
+        target[9] = (byte) (h2 >> 16);
+        target[10] = (byte) (h2 >> 8);
+        target[11] = (byte) (h2);
+
+        target[12] = (byte) (h3 >> 24);
+        target[13] = (byte) (h3 >> 16);
+        target[14] = (byte) (h3 >> 8);
+        target[15] = (byte) (h3);
+
+        target[16] = (byte) (h4 >> 24);
+        target[17] = (byte) (h4 >> 16);
+        target[18] = (byte) (h4 >> 8);
+        target[19] = (byte) (h4);
+
+        target[20] = (byte) (h5 >> 24);
+        target[21] = (byte) (h5 >> 16);
+        target[22] = (byte) (h5 >> 8);
+        target[23] = (byte) (h5);
+
+        target[24] = (byte) (h6 >> 24);
+        target[25] = (byte) (h6 >> 16);
+        target[26] = (byte) (h6 >> 8);
+        target[27] = (byte) (h6);
+
+        target[28] = (byte) (h7 >> 24);
+        target[29] = (byte) (h7 >> 16);
+        target[30] = (byte) (h7 >> 8);
+        target[31] = (byte) (h7);
+
+        solved[0] =  true;
+        this.nonce[0] = nonce;
+        return true;
+    }
+
+    @Override
+    public void run() {
+        int id = getGlobalId();
+        int difficulty = length / 100 + 24;
+        long end = id + start + mod * 1_000_000;
+
+        for (long i = start + id; i < end; i+= mod) {
+            if (hash(i, difficulty)) {
+                return;
+            }
+        }
+    }
+}
---
+++ src/java/Miner.java
@@ -0,0 +1,185 @@
+import com.amd.aparapi.Device;
+import com.amd.aparapi.OpenCLDevice;
+import com.google.common.io.BaseEncoding;
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.PropertyConfigurator;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class Miner {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InterruptedException {
+        PropertyConfigurator.configure(Miner.class.getResource("/config/log4j.properties"));
+
+        Miner miner
+//        = new Miner(
+//            "32",
+//            BaseEncoding.base16().lowerCase().decode("00000062b91299768d53751b24e61509de70c613297e2d04b86caa4462b63b89"),
+//            129
+//        )
+            ;
+
+        miner = pollHead();
+
+        while (true) {
+            Miner next = miner.mine();
+            if (next == null) {
+                miner = pollHead();
+            } else {
+                miner = next;
+            }
+        }
+    }
+
+    public static Miner pollHead() throws IOException {
+        //instantiates httpclient to make request
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        //url with the post data
+        HttpGet httpost = new HttpGet("http://6857coin.csail.mit.edu/head");
+
+        //convert parameters into JSON object
+        //Handles what is returned from the page
+        HttpResponse response = httpclient.execute(httpost);
+        String s = EntityUtils.toString(response.getEntity());
+        // create an ObjectMapper instance.
+        ObjectMapper mapper = new ObjectMapper();
+        // use the ObjectMapper to read the json string and create a tree
+        JsonNode node = mapper.readTree(s);
+        String hash = node.findValue("PrevHash").getTextValue();
+        int length = node.findValue("Length").getIntValue();
+        System.out.println("hash : " + hash + ", length: " + length);
+        return new Miner(
+            "32",
+            BaseEncoding.base16().lowerCase().decode(hash),
+            length + 1
+        );
+    }
+
+    private byte[] hash;
+    private int length;
+    private byte[] contentsBytes;
+    private String contents;
+
+    public Miner(String contents, byte[] hash, int length) {
+        this.hash = hash;
+        this.length = length;
+        this.contents = contents;
+        contentsBytes = contents.getBytes();
+    }
+
+    public Miner mine() throws NoSuchAlgorithmException, IOException, InterruptedException {
+        int proc = Runtime.getRuntime().availableProcessors();
+        System.out.println("procs: " + proc);
+        int threads = 256;
+        long offset = 0;
+        long mod = 16567;
+
+        byte[] lengthBytes = Ints.toByteArray(length);
+        byte[] block = new byte[hash.length + contentsBytes.length + 8 + lengthBytes.length];
+        System.arraycopy(hash, 0, block, 0, hash.length);
+        System.arraycopy(contentsBytes, 0, block, hash.length, contentsBytes.length);
+        System.arraycopy(lengthBytes, 0, block, hash.length + contentsBytes.length + 8, lengthBytes.length);
+
+        System.out.println(block.length + "," + contentsBytes.length);
+
+        long time = System.currentTimeMillis();
+        Device best = Device.best();
+        OpenCLDevice.DeviceSelector selector = new OpenCLDevice.DeviceSelector() {
+            @Override
+
+            public OpenCLDevice select(OpenCLDevice openCLDevice) {
+                System.out.println(openCLDevice.getPlatform().getVendor());
+                System.out.println(openCLDevice.getPlatform().getName());
+                System.out.println(openCLDevice.getPlatform().getVersion());
+                System.out.println(openCLDevice.getPlatform().toString());
+                if (openCLDevice.getType() == Device.TYPE.GPU && openCLDevice.getPlatform().getVendor().equals("NVIDIA")) {
+                    return openCLDevice;
+                }
+                return null;
+            }
+        };
+
+        System.out.println(best);
+
+        Device nvidia = OpenCLDevice.select(selector);
+
+        if (nvidia != null) {
+            best = nvidia;
+        }
+//        int count = 20;
+        for (long i = offset; true; i += mod * 1_000_000L) {
+            System.out.println(i);
+            GpuSha256 kernel = new GpuSha256(block, i, mod, length);
+            kernel.execute(best.createRange(threads));
+            System.out.println(kernel.getExecutionMode());
+            if (kernel.solved[0]) {
+                byte[] nonce = Longs.toByteArray(kernel.nonce[0]);
+                byte[] next = kernel.target;
+                System.out.println(BaseEncoding.base16().lowerCase().encode(next));
+                post(BaseEncoding.base16().lowerCase().encode(hash), contents, kernel.nonce[0], length);
+                System.exit(0);
+                return null;
+            }
+
+//            if (count == 0) {
+//                Thread.sleep(20000);
+//                count = 20;
+//            }
+//            count--;
+//            do poll
+            long cur = System.currentTimeMillis();
+            if (cur - time >= 30000) {
+                time = cur;
+                Miner miner = pollHead();
+                if (miner.length != length) {
+                    return miner;
+                }
+            }
+        }
+    }
+
+    public static void post(String hash, String contents, long nonce, int length) throws IOException {
+        //instantiates httpclient to make request
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        //url with the post data
+        HttpPost httpost = new HttpPost("http://6857coin.csail.mit.edu/add");
+
+        //convert parameters into JSON object
+        String fmt = "{\n" +
+            "  \"PrevHash\": \"%s\",\n" +
+            "  \"Contents\": \"%s\",\n" +
+            "  \"Nonce\": %d,\n" +
+            "  \"Length\": %d\n" +
+            "}";
+
+        System.out.println(String.format(fmt, hash, contents, nonce, length));
+        //passes the results to a string builder/entity
+        StringEntity se = new StringEntity(String.format(fmt, hash, contents, nonce, length));
+
+        //sets the post request as the resulting string
+        httpost.setEntity(se);
+        //sets a request header so the page receving the request
+        //will know what to do with it
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+
+        //Handles what is returned from the page
+        ResponseHandler responseHandler = new BasicResponseHandler();
+        System.out.println(httpclient.execute(httpost, responseHandler));
+    }
+
+
+}
