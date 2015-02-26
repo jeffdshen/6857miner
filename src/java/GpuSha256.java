38b7488dcd8c02165a8adbc0e1ae3b209898d773
jdshen <jdshen@mit.edu>
2015-02-26 06:49:54 -0500

    Licensing

--- src/java/GpuSha256.java
+++ src/java/GpuSha256.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2015 Ronald W Hoffman.
+ * Copyright 2015 Jeffrey D Shen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.amd.aparapi.Kernel;
 
 /**
-* SHA-256 hash algorithm for Monetary System currencies
+* SHA-256 hash algorithm for Monetary System currencies - modified for 6.857 Problem Set 2.
 *
 * GpuSha256 uses the Aparapi package to calculate hashes using the graphics card
 * GPU.  The Aparapi and OpenCL runtime libraries must be available in either the
